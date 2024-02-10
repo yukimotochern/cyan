ff59feca917b88533a0ec937cc7ec1d589213340
yukimoto <shtomcom@gmail.com>
2024-02-10 21:07:41 +0800

    move projects

---
+++ indie-card/game/db-jobs/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'indie-card-game-db-jobs',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/indie-card/game/db-jobs',
+};
